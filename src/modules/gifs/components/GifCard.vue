51f271d2204d3a7ab590b4df1f40ca869ca9b1e2
ErikaNunes <erikanunes0512@gmail.com>
2024-08-20 02:39:25 -0300

    feat: create gifs card  component

---
+++ src/modules/gifs/components/GifCard.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="p-4 bg-white rounded-md shadow-lg">
+    <div class="bg-gray-200 p-1 rounded-md">
+      <q-img
+        :src="props.gifUrl"
+        class="w-full h-full object-cover rounded-md"
+      />
+    </div>
+    <div class="flex justify-end mt-2">
+      <q-btn
+        icon="favorite"
+        v-if="!props.isFavoritePage"
+        @click="onButtonClick"
+        class="text-red-500"
+        flat
+      />
+      <q-btn
+        icon="close"
+        v-else
+        @click="onButtonClick"
+        class="text-gray-500"
+        flat
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps, defineEmits } from 'vue';
+
+const props = defineProps({
+  gifUrl: {
+    type: String,
+    required: true,
+  },
+  isFavoritePage: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const emit = defineEmits(['button-click']);
+
+function onButtonClick() {
+  emit('button-click');
+}
+</script>
+
+<style scoped></style>
