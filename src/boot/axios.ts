acff62bd32b2be1d484dd0e2d06d3615df51535b
ErikaNunes <erikanunes0512@gmail.com>
2024-08-20 01:31:07 -0300

    fix: define env and api key

--- src/boot/axios.ts
+++ src/boot/axios.ts
@@ -8,24 +8,20 @@ declare module '@vue/runtime-core' {
   }
 }
 
-// Be careful when using SSR for cross-request state pollution
-// due to creating a Singleton instance here;
-// If any client changes this (global) instance, it might be a
-// good idea to move this instance creation inside of the
-// "export default () => {}" function below (which runs individually
-// for each client)
-const api = axios.create({ baseURL: 'https://api.example.com' });
+const baseURL = import.meta.env.VITE_API_BASE_URL;
+// const apiKey = import.meta.env.VITE_GIPHY_API_KEY;
+
+const api = axios.create({
+  baseURL,
+  headers: {
+    'Content-Type': 'application/json',
+    // Authorization: `Bearer ${apiKey}`,
+  },
+});
 
 export default boot(({ app }) => {
-  // for use inside Vue files (Options API) through this.$axios and this.$api
-
   app.config.globalProperties.$axios = axios;
-  // ^ ^ ^ this will allow you to use this.$axios (for Vue Options API form)
-  //       so you won't necessarily have to import axios in each vue file
-
   app.config.globalProperties.$api = api;
-  // ^ ^ ^ this will allow you to use this.$api (for Vue Options API form)
-  //       so you can easily perform requests against your app's API
 });
 
 export { api };
